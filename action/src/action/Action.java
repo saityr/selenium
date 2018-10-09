d94f62dd41c6472b3ad1b0a55eb1e2c19d6f9446
Training_C2a.05.01 <Training_C2a.05.01@CDC2-D-CLNRXJ2.dir.svc.accenture.com>
2018-10-09 14:51:22 +0530

    commit

---
+++ action/src/action/Action.java
@@ -0,0 +1,12 @@
+package action;
+
+public class Action {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.get("")
+		
+		
+	}
+
+}
